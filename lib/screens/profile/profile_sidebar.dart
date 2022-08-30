1986f43432d34a2691678efd792597b6341230e2
doublealpha21 <praise8fakorede@gmail.com>
2022-08-30 10:02:23 +0100

    expansion

--- lib/screens/profile/profile_sidebar.dart
+++ lib/screens/profile/profile_sidebar.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:playback_ui/utils/colors.dart';
-import 'package:playback_ui/widgets/custom_button.dart';
 import 'package:playback_ui/widgets/horizontal_space.dart';
 import 'package:playback_ui/widgets/vertical_space.dart';
-import 'package:playback_ui/widgets/common_app_bar.dart';
-import 'package:playback_ui/utils/textfield.dart';
-import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class ProfileSidebar extends StatelessWidget {
   const ProfileSidebar({Key? key}) : super(key: key);
@@ -125,6 +121,7 @@ class ProfileSidebar extends StatelessWidget {
                         )
                       ],
                     ),
+                    VerticalSpace(),
                     VerticalSpace(),
                     Row(
                       children: const [
---
+++ lib/widgets/card_expansion.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import'package:playback_ui/utils/colors.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
+
+
+class FaqShowBox extends StatelessWidget {
+  final String title;
+  final String textData;
+
+  const FaqShowBox({
+    Key? key,
+    required this.title,
+    required this.textData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTileCard(
+      expandedColor: const Color(0xff202125),
+      baseColor: ColorResources.PrimaryGreen,
+      elevation: 0,
+      key: key,
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: ColorResources.White,
+          fontWeight: FontWeight.w400,
+
+        ),
+        ),
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            // const EdgeInsets.all(20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: const Color(0xFFF5F8FA),
+              // ColorResources.kColorPrimary.withOpacity(.2),
+            ),
+            child: Text(
+              textData,
+              style:  const TextStyle(
+                color: ColorResources.White,
+                fontWeight: FontWeight.w400,
+
+              ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
